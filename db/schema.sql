0d0bef876e6ee9fef6ab2bbef14c3bae6270c2ec
LV Magee <lvmagee@gmail.com>
2017-05-31 16:42:04 -0400

    Updated code

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE Burgers_DB;
 CREATE TABLE burgers (
     item_id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NULL,
-    devoured ENUM('TRUE','FALSE'),
+    devoured BOOLEAN DEFAULT false,
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (item_id)
 );
